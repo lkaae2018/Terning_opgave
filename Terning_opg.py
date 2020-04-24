41c4304b9ccb815a23cd488266f764830e648a34
Lasse Kaae <lak@djhhadsten.dk>
2020-04-24 12:02:19 +0200

    Ny terningopg

--- Terning_opg.py
+++ Terning_opg.py
@@ -4,10 +4,17 @@ import random
 # Der skal trykkes på en tast mellem terningkast
 # Udskriv antal øjne og stillingen mellem dig og maskinen
 # ved hvert kast
+# Tilslut skal i lave terningerne grafik
+# Opret en .py file, hvor terningerne skal importeres fra
+# _____________
+# |  O     O   |
+# |     O      |
+# |  O      O  |
+# ______________
 
 def menu():
     print("Du spiller mod maskinen")
-    x=input("Tryk på en tast + enter for starte")
+    x=input("Tryk på en enter for starte")
 
 Dig=0
 Maskine=0
